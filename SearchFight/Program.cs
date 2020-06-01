11c0364040ec3b03e917c31729ecd7825a29b0ed
Wilber Choque <wchoqueitusaca@outlook.com>
2020-06-01 11:15:09 -0500

    Adding appsettings file

--- SearchFight/Program.cs
+++ SearchFight/Program.cs
@@ -11,15 +11,15 @@ namespace SearchFight
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
-            {
-                Console.WriteLine("No terms were specified for the Search Fight. Please execute again with the search terms.");
-                return;
-            }
-            //Console.WriteLine("Executing Search Fight....");
-            //Console.WriteLine("Type words you want to compare");
-            //var searchableWords = Console.ReadLine();
-            //args = searchableWords.Split(" ");
+            //if (args.Length == 0)
+            //{
+            //    Console.WriteLine("No terms were specified for the Search Fight. Please execute again with the search terms.");
+            //    return;
+            //}
+            Console.WriteLine("Executing Search Fight....");
+            Console.WriteLine("Type words you want to compare");
+            var searchableWords = Console.ReadLine();
+            args = searchableWords.Split(" ");
             MainAsync(args).GetAwaiter().GetResult();
             Console.Read();
         }
@@ -31,8 +31,6 @@ namespace SearchFight
             var searchFightService = provider.GetRequiredService<ISearchFightService>();
             await searchFightService.SearchFightAsync(args.ToList());
             searchFightService.GetReports().ForEach(report => Console.WriteLine(report.Result));
-            //Console.WriteLine(SearchFightService.GetCompleteReports());
-            //searchFightService.PrintCompleteReports();
         }
     }
 }
